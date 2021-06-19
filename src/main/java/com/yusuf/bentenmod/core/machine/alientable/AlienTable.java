377d7fb81f955935908b2d3c68885b5eb565cedb
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-19 16:26:24 +0100

    Update AlienTable.java

--- src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTable.java
+++ src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTable.java
@@ -12,10 +12,7 @@ import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
 import net.minecraft.inventory.container.WorkbenchContainer;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.state.StateContainer;
 import net.minecraft.stats.Stats;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.IWorldPosCallable;
@@ -23,10 +20,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
-import net.minecraftforge.fml.network.NetworkHooks;
 
 public class AlienTable extends Block {
     private static final ITextComponent CONTAINER_TITLE = new TranslationTextComponent("container.crafting");
