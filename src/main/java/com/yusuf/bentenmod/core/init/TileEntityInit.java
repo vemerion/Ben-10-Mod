7664cc81aa084b99d25142fc2c55e14d49544865
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-19 15:10:28 +0100

    update

--- src/main/java/com/yusuf/bentenmod/core/init/TileEntityInit.java
+++ src/main/java/com/yusuf/bentenmod/core/init/TileEntityInit.java
@@ -1,6 +1,7 @@
 package com.yusuf.bentenmod.core.init;
 
 import com.yusuf.bentenmod.Main;
+import com.yusuf.bentenmod.core.machine.alientable.AlienTableTileEntity;
 import com.yusuf.bentenmod.core.machine.bententable.TableTileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
@@ -10,6 +11,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class TileEntityInit {
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITY = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, Main.MOD_ID);
 
-    public static final RegistryObject<TileEntityType<TableTileEntity>> TABLE_TE = TILE_ENTITY.register("table", () -> TileEntityType.Builder.of(TableTileEntity::new, BlockInit.TABLE_BLOCK.get()).build(null));
+    public static final RegistryObject<TileEntityType<TableTileEntity>> TABLE_TE = TILE_ENTITY.register("table_block", () -> TileEntityType.Builder.of(TableTileEntity::new, BlockInit.TABLE_BLOCK.get()).build(null));
+    public static final RegistryObject<TileEntityType<AlienTableTileEntity>> ALIEN_TABLE = TILE_ENTITY.register("alien_table", () -> TileEntityType.Builder.of(TableTileEntity::new, BlockInit.ALIEN_TABLE.get()).build(null));
+
 
 }
