295188689b5639fa529917bb5929935783ff1cdd
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-19 17:23:14 +0100

    Update AlienTable.java

--- src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTable.java
+++ src/main/java/com/yusuf/bentenmod/core/machine/alientable/AlienTable.java
@@ -1,14 +1,9 @@
 
 package com.yusuf.bentenmod.core.machine.alientable;
 
-import com.yusuf.bentenmod.core.init.TileEntityInit;
-import com.yusuf.bentenmod.core.machine.bententable.TableTileEntity;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
 import net.minecraft.inventory.container.WorkbenchContainer;
@@ -21,9 +16,8 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ToolType;
-
+/** @see CraftingTableBlock */
 public class AlienTable extends Block {
     private static final ITextComponent CONTAINER_TITLE = new TranslationTextComponent("container.crafting");
 
@@ -51,11 +45,18 @@ public class AlienTable extends Block {
     public INamedContainerProvider getMenuProvider(BlockState p_220052_1_, World p_220052_2_, BlockPos p_220052_3_) {
         return new SimpleNamedContainerProvider((p_220270_2_, p_220270_3_, p_220270_4_) -> {
             return new WorkbenchContainer(p_220270_2_, p_220270_3_, IWorldPosCallable.create(p_220052_2_, p_220052_3_));
+
         }, CONTAINER_TITLE);
     }
-        
 
-}
+    }
+
+
+
+
+
+
+
 
 
 
